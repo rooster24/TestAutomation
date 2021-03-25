8488c0354689e78a1f3860777cc86cac47b7b413
rooster24 <ps23wi@gmail.com>
2021-03-25 14:58:31 -0500

    Setup Test; Launch browser to expedia.com

--- ExpediaAutomation/ExpediaAutomation/StartupClass.cs
+++ ExpediaAutomation/ExpediaAutomation/StartupClass.cs
@@ -1,14 +1,50 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 using System;
+using System.Text;
 
 namespace ExpediaAutomation
 {
-    [TestClass]
+    [TestFixture]
     public class StartupClass
     {
-        [TestMethod]
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        //private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.expedia.com/";
+            verificationErrors = new StringBuilder();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            NUnit.Framework.Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
         public void TestMethod1()
         {
+            driver.Navigate().GoToUrl(baseURL);
+
         }
+
+
     }
 }
