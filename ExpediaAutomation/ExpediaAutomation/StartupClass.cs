236c3e687c0eac29d4cdd3d3fc5cc50339747977
rooster24 <ps23wi@gmail.com>
2021-03-25 14:31:39 -0500

    Created Project

---
+++ ExpediaAutomation/ExpediaAutomation/StartupClass.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ExpediaAutomation
+{
+    [TestClass]
+    public class StartupClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
